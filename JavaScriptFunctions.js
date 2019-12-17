99fb61e13cb74125e25fc52022823ba9e01cc0b6
meetmmpatel <meetmmpatel@gmail.com>
2019-12-17 12:28:38 -0500

    Added types of funcrtions

---
+++ JavaScriptFunctions.js
@@ -0,0 +1,17 @@
+
+
+//type of functions
+
+function getName(params) {
+        //void functions
+}
+
+function getArgs(args) {
+        return args
+        // this functions with return type
+}
+
+const someName = (firstName, lastName) => {
+        return firstName + lastName;
+}
+
